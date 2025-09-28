420ba023583df13fab5a42df727b0db3b27cda2c
tuandevdt <doanthanhtuan1998@gmail.com>
2025-09-28 13:45:19 +0700

    refactor: fix logic search product & interface api

--- src/pages/Products/ProductsPage.tsx
+++ src/pages/Products/ProductsPage.tsx
@@ -72,9 +72,11 @@ const ProductsPage: React.FC = () => {
     setFilters(prev => ({ ...prev, page }));
   };
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const search = e.target.value;
-    setFilters(prev => ({ ...prev, search, page: 1 }));
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const search = (e.target as HTMLInputElement).value;
+      setFilters(prev => ({ ...prev, search, page: 1 }));
+    }
   };
 
   return (
@@ -100,11 +102,7 @@ const ProductsPage: React.FC = () => {
           <TextField
             size='small'
             placeholder='Search websites...'
-            onKeyDown={e => {
-              if (e.key === 'Enter') {
-                handleSearch(e);
-              }
-            }}
+            onKeyDown={handleKeyDown}
           />
         </Box>
 
